eeaa43c431cdcffeca6c9e233b8945d7928f11d8
lRezaJl <storereza95@gmail.com>
2024-09-09 15:26:27 +0330

    responaive account

--- Components/Account.jsx
+++ Components/Account.jsx
@@ -17,11 +17,15 @@ const Card = () => {
   const [router, setRouter] = useState(null);
   const [activeComponent, setActiveComponent] = useState("ReserveList");
 
-  const handleComponentChange = (ReserveList) => {
-    setActiveComponent(ReserveList);
+  const handleComponentChange = (componentName) => {
+    setActiveComponent(componentName);
   };
 
   const handleFocus = (cardIndex) => {
+    if (window.innerWidth >= 1024) {
+      // در صفحه‌های بزرگتر از 1024 پیکسل تغییری در اندازه نده
+      return;
+    }
     if (cardIndex === 1) {
       setFocusedCard([1]);
     } else {
@@ -31,15 +35,13 @@ const Card = () => {
 
   const navigateTo = (path) => {
     if (router) {
-      router.push(path); // فقط وقتی router تعریف شده باشد استفاده می‌شود
+      router.push(path);
     }
   };
 
   function LogOut() {
     localStorage.removeItem("access");
     localStorage.removeItem("refresh");
-
-    // Redirect to the login page or any other page
     window.location.href = "/home";
   }
 
@@ -48,11 +50,12 @@ const Card = () => {
       dir="rtl"
       className="w-full h-screen rounded-box bg-gray-300 flex flex-row p-1"
     >
+      {/* بخش 1 */}
       <div
         className={`relative flex-1 h-full overflow-hidden rounded-btn transition-all duration-500 bg-gray-300 flex justify-center items-start min-w-5 ${
           focusedCard.includes(1)
-            ? "flex-[0.5] max-sm:flex-[14] max-md:flex-[2] max-lg:flex-[1] max-xl:flex-[0.75]"
-            : "transition-all duration-500 cursor-pointer"
+            ? "flex-[0.5] max-sm:flex-[10] max-md:flex-[10] max-lg:flex-[8] max-xl:flex-[0.75]"
+            : "cursor-pointer"
         }`}
         onClick={() => handleFocus(1)}
       >
@@ -60,21 +63,21 @@ const Card = () => {
           className={`absolute z-40 flex justify-center items-center min-w-[52rem] min-h-20 mt-2 p-2 transform transition-all duration-500 ${
             focusedCard.includes(1)
               ? "top-0"
-              : "w-full h-full backdrop-blur-lg backdrop-saturate-150 drop-shadow-hardWhite cursor-pointer"
+              : "max-lg:w-full max-lg:h-full max-lg:backdrop-blur-lg"
           }`}
-          onClick={() => handleFocus(1)}
         >
           <span
-            className={`hover:rotate-0 text-center text-gray-800 text-xl font-bold transform transition-all duration-500 ${
+            className={`text-center text-gray-800 text-xl font-bold transform transition-all duration-500 ${
               focusedCard.includes(1)
                 ? "rotate-0"
-                : "-rotate-90 top-[50%] cursor-pointer text-2xl max-sm:text-lg"
+                : "lg:hidden -rotate-90 top-[50%] cursor-pointer text-2xl max-sm:text-lg"
             }`}
           >
             منو
           </span>
         </div>
-        <div className="flex flex-col w-full mt-5 md:pr-3 md:pl-2 pr-1 overflow-hidden">
+        {/* محتوای بخش 1 */}
+        <div className="flex flex-col w-full mt-5 md:pr-3 pl-2 pr-1 overflow-hidden">
           <div className="flex my-10">
             <Image
               className="w-[112px]"
@@ -84,7 +87,6 @@ const Card = () => {
               height={67}
             />
           </div>
-
           <div className="relative flex flex-row justify-start items-start gap-4 p-3 bg-gray-400 rounded-box">
             <div className="avatar">
               <div className="w-16 rounded-full">
@@ -101,31 +103,32 @@ const Card = () => {
               <CiEdit />
             </button>
           </div>
-          <nav className="flex flex-col justify-center items-start gap-10 my-10 lg:mr-14">
+          {/* منوهای ناوبری */}
+          <nav className="flex flex-col justify-center items-start gap-10 my-10 lg:mr-2">
             <Link
               href="/"
-              className="btn bg-transparent border-none shadow-none hover:bg-gray-300 drop-shadow-3xlGray hover:shadow-navbox flex-nowrap flex flex-row justify-start items-start gap-5 text-gray-700"
+              className="btn bg-transparent border-none hover:bg-gray-300 flex-nowrap flex flex-row gap-8 text-gray-700"
             >
               <IoHome className="text-4xl font-bold" />
               <p className="text-4xl font-bold tracking-wide">خانه</p>
             </Link>
             <button
               onClick={() => handleComponentChange("ReserveList")}
-              className="btn bg-transparent border-none shadow-none hover:bg-gray-300 drop-shadow-3xlGray hover:shadow-navbox flex-nowrap flex flex-row justify-start items-start gap-5 text-gray-700"
+              className="btn bg-transparent border-none hover:bg-gray-300 flex-nowrap flex flex-row gap-8 text-gray-700"
             >
               <FaClipboardList className="text-4xl font-bold" />
-              <p className="text-4xl font-bold tracking-wide ">رزرو ها</p>
+              <p className="text-4xl font-bold tracking-wide">رزرو ها</p>
             </button>
             <button
               onClick={() => handleComponentChange("Payments")}
-              className="btn bg-transparent border-none shadow-none hover:bg-gray-300 drop-shadow-3xlGray hover:shadow-navbox flex-nowrap flex flex-row justify-start items-start gap-5 text-gray-700"
+              className="btn bg-transparent border-none hover:bg-gray-300 flex-nowrap flex flex-row gap-8 text-gray-700"
             >
               <SiCashapp className="text-4xl font-bold" />
               <p className="text-4xl font-bold tracking-wide">پرداخت</p>
             </button>
             <button
-              className="btn bg-transparent border-none shadow-none drop-shadow-3xlRed hover:bg-gray-300 hover:shadow-navbox  flex-nowrap flex flex-row justify-start items-start gap-5 text-coralRed"
-              onClick={() => LogOut()} // Navigate to the logout page
+              onClick={LogOut}
+              className="btn bg-transparent border-none hover:bg-gray-300 flex-nowrap flex flex-row gap-8 text-coralRed"
             >
               <FaPowerOff className="text-4xl font-bold" />
               <p className="text-4xl font-bold tracking-wide">خروج</p>
@@ -134,27 +137,27 @@ const Card = () => {
         </div>
       </div>
 
+      {/* بخش 2 */}
       <div
-        className={`relative flex-1 h-full overflow-hidden rounded-btn transition-all duration-500 bg-gray-800 flex justify-center items-start min-w-5  ${
+        className={`relative flex-1 h-full overflow-hidden rounded-btn transition-all duration-500 bg-gray-800 flex justify-center items-start min-w-5 ${
           focusedCard.includes(2)
-            ? "max-sm:flex-[14] sm:flex-[2] xl:flex-[3]"
-            : "transition-all duration-500 cursor-pointer"
+            ? "max-sm:flex-[10] max-md:flex-[10] max-lg:flex-[8] lg:flex-[2.7] xl:flex-[3]"
+            : "cursor-pointer"
         }`}
         onClick={() => handleFocus(2)}
       >
         <div
           className={`absolute z-50 flex justify-center items-center min-w-[52rem] min-h-20 p-2 transform transition-all duration-500 text-gray-300 text-xl font-bold ${
             focusedCard.includes(2)
-              ? "rotate-0 top-0 "
-              : "w-full h-full backdrop-blur-lg backdrop-saturate-150 drop-shadow-hardDark cursor-pointer"
+              ? "rotate-0 top-0"
+              : "max-lg:w-full max-lg:h-full max-lg:backdrop-blur-lg"
           }`}
-          onClick={() => handleFocus(2)}
         >
           <span
-            className={`hover:rotate-0 text-center transform transition-all duration-500 tracking-wide ${
+            className={`text-center transform transition-all duration-500 ${
               focusedCard.includes(2)
                 ? "rotate-0"
-                : "-rotate-90 top-[50%] cursor-pointer"
+                : "lg:hidden -rotate-90 top-[50%] cursor-pointer"
             }`}
           >
             {activeComponent === "ReserveList" && "رزرو ها"}
@@ -162,6 +165,7 @@ const Card = () => {
           </span>
         </div>
 
+        {/* محتوای بخش 2 */}
         <div className="p-5 mt-16 w-full">
           {activeComponent === "ReserveList" && <ReserveList />}
           {activeComponent === "Payments" && <Payments />}
