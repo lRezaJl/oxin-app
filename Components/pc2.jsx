aebc2fdbb7da8bfc41acf9aa748108f986aa3d0f
lRezaJl <storereza95@gmail.com>
2024-09-20 05:49:12 +0330

    add imgs

---
+++ Components/pc2.jsx
@@ -0,0 +1,12 @@
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import asset1 from "../public/images/Asset1.png";
+import asset3 from "../public/images/Asset3.png";
+
+export default function pcs() {
+  return <div>
+    <div></div>
+    <img src={asset1} alt="" />
+  </div>;
+}
